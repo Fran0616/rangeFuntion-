24ade0ff78dbd7815ba0697b0a2484e89365e034
Fran0616 <59398607+Fran0616@users.noreply.github.com>
2020-08-22 14:20:00 -0400

    Add files via upload

---
+++ rangeFunction .py
@@ -0,0 +1,8 @@
+#the range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 by default. and stops before a specefied number.
+#range(start, stop, step)
+#start - specefied which position to start
+#stop - which position to stop 
+#step the interger number specefied the incrementation.
+for i in range(5, 35 , 15):
+    print(i)
+    
